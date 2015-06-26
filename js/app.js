2232896c9fa9c11fb7abe968b302b438aa5d7e59
Edgar Mueller <edgar.m.mueller@gmail.com>
2015-06-26 16:19:34 +0200

    formatting

--- js/app.js
+++ js/app.js
@@ -3,35 +3,35 @@
 var app = angular.module('jsonforms-seed', []);
 app.controller('MyController', ['$scope', function($scope) {
 
-	$scope.schema = {
-    	"type": "object",
-    	"properties": {
-      		"name": {
-        		"type": "string"
-      		},
-    		"age": {
-      			"type": "integer"
-    		},
-    		"gender": {
-      			"type": "string",
-      			"enum": [ "Male", "Female" ]
-    		},
-    		"birthDate" {
-      			"type": "date"
-    		}
-  		}
-  	};
+    $scope.schema = {
+        "type": "object",
+        "properties": {
+            "name": {
+                "type": "string"
+            },
+            "age": {
+                "type": "integer"
+            },
+            "gender": {
+                "type": "string",
+                "enum": ["Male", "Female"]
+            },
+            "birthDate" {
+                "type": "date"
+            }
+        }
+    };
 
-	$scope.data = {
-    	name:   'John Doe',
-     	age:    36,
-     	height: 1.85
-     	gender: 'Male',
-   	};
+    $scope.data = {
+        name: 'John Doe',
+        age: 36,
+        height: 1.85
+        gender: 'Male',
+    };
 
-	$scope.formattedData = function() {
-		return JSON.stringify(data);
-	};
+    $scope.formattedData = function() {
+        return JSON.stringify(data);
+    };
 
     // TODO: define UI schema
 }]);
