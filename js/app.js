43dc1e59344f14fae6f9acfeabdc90fed3d70a07
Edgar Mueller <edgar.m.mueller@gmail.com>
2015-06-26 16:16:55 +0200

    added some predefined data to the controller

--- js/app.js
+++ js/app.js
@@ -1,3 +1,37 @@
 'use strict';
 
 var app = angular.module('jsonforms-seed', []);
+app.controller('MyController', ['$scope', function($scope) {
+
+	$scope.schema = {
+    	"type": "object",
+    	"properties": {
+      		"name": {
+        		"type": "string"
+      		},
+    		"age": {
+      			"type": "integer"
+    		},
+    		"gender": {
+      			"type": "string",
+      			"enum": [ "Male", "Female" ]
+    		},
+    		"birthDate" {
+      			"type": "date"
+    		}
+  		}
+  	};
+
+	$scope.data = {
+    	name:   'John Doe',
+     	age:    36,
+     	height: 1.85
+     	gender: 'Male',
+   	};
+
+	$scope.formattedData = function() {
+		return JSON.stringify(data);
+	};
+
+    // TODO: define UI schema
+}]);
