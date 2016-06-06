85f98aba0d142cf401ec9462a7ca2c29c77a654d
Eugen Neufeld <eneufeld@eclipsesource.com>
2016-06-06 09:49:31 +0200

    changed seed to use npm and new emfToJson exporter

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # JSONForms Seed
-This is a simple template project to get started with JSONForms. 
+This is a simple template project to get started with JSONForms.
 It contains an `index.html` which includes the `jsonforms` directive as well as some JS files that contain the JSON schema, the UI schema and the data to be displayed.
 
 ## Usage
-Just run `bower install` after you've cloned the repo and you're set.
+Just run `npm start` after you've cloned the repo and you're set.
--- js/schema.js
+++ js/schema.js
@@ -1,26 +1,24 @@
 'use strict';
 
 var app = angular.module('jsonforms-seed');
-app.factory('SchemaService', function() {
-    return {
-        schema: {
-            "type": "object",
-            "properties": {
-                "name": {
-                    "type": "string",
-                    "minLength": 3
-                },
-                "age": {
-                    "type": "integer"
-                },
-                "gender": {
-                    "type": "string",
-                    "enum": ["Male", "Female"]
-                },
-                "height": {
-                    "type": "number"
-                }
+app.value('Schema',
+    {
+        "type": "object",
+        "properties": {
+            "name": {
+                "type": "string",
+                "minLength": 3
+            },
+            "age": {
+                "type": "integer"
+            },
+            "gender": {
+                "type": "string",
+                "enum": ["Male", "Female"]
+            },
+            "height": {
+                "type": "number"
             }
         }
     }
-});
+);
